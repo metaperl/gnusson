3c0bb0077e29054396ed2720d0f90c32354c7115
Terrence Brannon <metaperl@gmail.com>
2011-05-10 12:56:05 -0400

    1st commit

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.gnusson.boolpref", false);
+pref("extensions.gnusson.intpref", 0);
+pref("extensions.gnusson.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.gnusson@metaperl.org.description", "chrome://gnusson/locale/overlay.properties");
